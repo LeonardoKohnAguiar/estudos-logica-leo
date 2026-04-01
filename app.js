1c6039331198c68fd50d7fde29390a807491002d
Leonardo Kohn Aguiar <leonardok.aguiar@Yahoo.com.br>
2026-03-31 22:52:54 -0300

    feat: monitoramento do data center e ignorando modulos

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (nivelPermissao === "admin") {
 // --------------------------------------------------------------------------------------------------------------------
 
 // Simulação de um Ativo Detalhado 
-const switchCore = {
+/*const switchCore = {
     nome: "Switch Core Alphaville",
     marca: "Cisco",
     portasOcupadas: 42,
@@ -27,4 +27,38 @@ if (switchCore.portasOcupadas > 40) {
     console.log("ALERTA: Poucas portas disponiveis neste switch!");
 } else {
     console.log("Capacidade do Switch está estável.");
+}*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+const chalk = require('chalk');
+
+function monitorarEquipamento(nome, temperatura) {
+    if (temperatura > 75) {
+        console.log(chalk.bgWhite.red.bold(`🚨 ALERTA CRÍTICO: ${temperatura}°C - ${nome}`));
+    } else if (temperatura > 50) {
+        console.log(chalk.yellow(`⚠️ ATENÇÃO: ${temperatura}°C - ${nome}`));
+    } else {
+        console.log(chalk.green(`✅ ESTÁVEL: ${temperatura}°C - ${nome}`));
+    }
 }
+
+// ==========================================
+// NOVO: Array de Objetos (Nosso "Banco de Dados" local)
+// ==========================================
+const dataCenter = [
+    { equipamento: "Switch Core Alphaville", temp: 35 },
+    { equipamento: "Roteador BGP Piso 1", temp: 62 },
+    { equipamento: "Servidor de Banco de Dados", temp: 85 },
+    { equipamento: "Firewall Principal", temp: 45 },
+    { equipamento: "Nobreak Geral", temp: 78 }
+];
+
+console.log(chalk.magenta.bold("\n=== 🔄 INICIANDO VARREDURA DO DATACENTER ==="));
+
+// O Loop Mágico: Para cada 'item' dentro de 'dataCenter', execute a função
+dataCenter.forEach(item => {
+    monitorarEquipamento(item.equipamento, item.temp);
+});
+
+console.log(chalk.magenta.bold("=== ✅ VARREDURA CONCLUÍDA ===\n"));
